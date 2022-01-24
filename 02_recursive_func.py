49863744b55018e8f265600409b3500a866f19c0
YukiarC <yuukihagino@gmail.com>
2022-01-24 21:21:53 +0900

    feat: added gcd

--- 02_recursive_func.py
+++ 02_recursive_func.py
@@ -1,8 +1,14 @@
-def recursive_func_sum(n: int):
+def recursive_sum(n: int):
     if n == 0:
         return 0
-    return n + recursive_func_sum(n - 1)
+    return n + recursive_sum(n - 1)
+
+
+def recursive_gcd(m: int, n: int):
+    if n == 0:
+        return m
+    return recursive_gcd(n, m % n)
 
 
 if __name__ == '__main__':
-    print(recursive_func_sum(10))
+    print(recursive_gcd(105, 15))
